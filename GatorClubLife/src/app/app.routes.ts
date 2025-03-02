6f47cd6cfefb1be605da3d51f52fbb2176e8793e
deekshitaak <d.kommi@ufl.edu>
2025-03-02 01:42:28 -0500

    Created a registration page

--- GatorClubLife/src/app/app.routes.ts
+++ GatorClubLife/src/app/app.routes.ts
@@ -3,12 +3,14 @@ import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
 import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 import { authGuard } from './auth.guard';
 import { EventsComponent } from './events/events.component'; // Import EventsComponent
 
 export const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' }, // Default route redirects to login
   { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent }, // Add the /register route
   { path: 'home', component: HomeComponent, canActivate: [authGuard] },
   { path: 'about', component: AboutComponent, canActivate: [authGuard] },
   { path: 'contact', component: ContactComponent, canActivate: [authGuard] },
