7087102edf3b7239a49107f8d3a65082a7eb0adf
AbhignaNimmagadda <Abhigna.Nimmagad@ufl.edu>
2025-03-01 23:58:08 -0500

    Updated Login Compoonent for the API endpoint integration

--- GatorClubLife/src/main.ts
+++ GatorClubLife/src/main.ts
@@ -1,15 +1,21 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 import { HomeComponent } from './app/home/home.component';
 import { AboutComponent } from './app/about/about.component';
+import { LoginComponent } from './app/login/login.component';
 import { provideRouter, Routes } from '@angular/router';
+import { appConfig } from './app/app.config';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
   { path: 'about', component: AboutComponent },
 ];
 
-
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [
+    ...appConfig.providers,
+    provideRouter(routes)
+  ]
+})
+.catch((err) => console.error(err));
