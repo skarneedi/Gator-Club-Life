3ee4ace7faead4e64e7ee04e666f1b03dc07854b
skarneedi <skarneedi@ufl.edu>
2025-03-22 21:02:29 -0400

    Front-end changes supporting Session Management and Logout

--- GatorClubLife/src/app/app.component.ts
+++ GatorClubLife/src/app/app.component.ts
@@ -1,40 +1,57 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Router, NavigationEnd } from '@angular/router';
 import { AnnouncementsComponent } from './announcements/announcements.component';
+import { AuthService } from './auth.service';
+import { Observable } from 'rxjs';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
   standalone: true,
-  imports: [CommonModule, RouterModule, AnnouncementsComponent],
+  imports: [CommonModule, RouterModule, AnnouncementsComponent, HttpClientModule],
 })
-export class AppComponent {
-  /* title = 'abhi'; */
+export class AppComponent implements OnInit {
   title = 'GatorClubLife';
-  isLoggedIn: boolean = false;
-  showAnnouncements: boolean = false; // Default: Hide Announcements
+  isLoggedIn$!: Observable<boolean>;
+  userName$!: Observable<string>;
+  showAnnouncements: boolean = false;
 
-  constructor(private router: Router) {
-    if (typeof localStorage !== 'undefined') {
-      this.isLoggedIn = !!localStorage.getItem('isLoggedIn'); // Check login status
-    }
-
-    // Listen for route changes
+  constructor(private router: Router, private authService: AuthService, private http: HttpClient) {
     this.router.events.subscribe((event) => {
       if (event instanceof NavigationEnd) {
-        // Show Announcements ONLY on '/home'
         this.showAnnouncements = event.url === '/home' || event.url === '/home/';
+        console.log('[AppComponent] NavigationEnd, showAnnouncements:', this.showAnnouncements);
       }
     });
   }
 
+  ngOnInit(): void {
+    this.isLoggedIn$ = this.authService.isLoggedIn$;
+    this.userName$ = this.authService.userName$;
+    console.log('[AppComponent] Subscribed to AuthService observables');
+    this.isLoggedIn$.subscribe((loggedIn) => {
+      console.log('[AppComponent] isLoggedIn changed to:', loggedIn);
+    });
+    this.userName$.subscribe((name) => {
+      console.log('[AppComponent] userName changed to:', name);
+    });
+  }
+
   logout() {
-    if (typeof localStorage !== 'undefined') {
-      localStorage.removeItem('isLoggedIn'); // Remove login status
-    }
-    this.isLoggedIn = false;
-    this.router.navigate(['/login']); // Redirect to login page
+    console.log('[AppComponent] Logging out user');
+    this.http.post('http://localhost:8080/logout', {}).subscribe({
+      next: () => {
+        console.log('[AppComponent] Logout successful from backend');
+        this.authService.clearUser();
+        this.router.navigate(['/login']);
+        console.log('[AppComponent] User logged out, navigated to login');
+      },
+      error: (error) => {
+        console.error('[AppComponent] Logout error:', error);
+      }
+    });
   }
 }
