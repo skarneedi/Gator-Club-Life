ead708a5e65ccc85abad1f9dd259127c334fdd2a
AbhignaNimmagadda <abhigna.nimmagad@ufl.edu>
2025-03-27 19:03:16 -0400

    PermitsPage

---
+++ GatorClubLife/src/app/permits/permits.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-permits',
+  imports: [],
+  templateUrl: './permits.component.html',
+  styleUrl: './permits.component.css'
+})
+export class PermitsComponent {
+
+}
