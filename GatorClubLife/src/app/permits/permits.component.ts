2d0d3851354a85fa678d1fd4c58bad2929681fe1
AbhignaNimmagadda <abhigna.nimmagad@ufl.edu>
2025-04-14 20:37:22 -0400

    Permits Page

--- GatorClubLife/src/app/permits/permits.component.ts
+++ GatorClubLife/src/app/permits/permits.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-permits',
-  imports: [],
   templateUrl: './permits.component.html',
-  styleUrl: './permits.component.css'
+  styleUrls: ['./permits.component.css'],
 })
 export class PermitsComponent {
+  constructor(private router: Router) {}
 
+  // Method to navigate to the appropriate form page
+  navigateToForm(permitType: string): void {
+    console.log(`Navigating to ${permitType} form...`);
+    // Example routing logic:
+    this.router.navigate(['/forms', permitType]);
+  }
 }
