063f3e36ce3e6ca30d95952bb64ddc7e9dc93323
AbhignaNimmagadda <abhigna.nimmagad@ufl.edu>
2025-04-21 21:58:09 -0400

    Completed cypress testing for FrontEnd work-flow

--- GatorClubLife/cypress/support/commands.ts
+++ GatorClubLife/cypress/support/commands.ts
@@ -1,37 +1,31 @@
 /// <reference types="cypress" />
-// ***********************************************
-// This example commands.ts shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
-//
-// declare global {
-//   namespace Cypress {
-//     interface Chainable {
-//       login(email: string, password: string): Chainable<void>
-//       drag(subject: string, options?: Partial<TypeOptions>): Chainable<Element>
-//       dismiss(subject: string, options?: Partial<TypeOptions>): Chainable<Element>
-//       visit(originalFn: CommandOriginalFn, url: string, options: Partial<VisitOptions>): Chainable<Element>
-//     }
-//   }
-// }
+
+Cypress.Commands.add('login', () => {
+    cy.session('skarneed@ufl.edu', () => {
+      cy.visit('http://localhost:4200/login');
+  
+      cy.get('input[name="username"]').type('skarneedi@ufl.edu');
+      cy.get('input[name="password"]').type('123');
+  
+      cy.get('button[type="submit"]').click();
+  
+      // Optional debug
+      cy.get('body').then($body => {
+        if ($body.text().includes('Invalid email') || $body.text().includes('Incorrect password')) {
+          throw new Error('🚫 Login failed');
+        }
+      });
+  
+      cy.url().should('not.include', '/login');
+    });
+  });
+  
+  declare global {
+    namespace Cypress {
+      interface Chainable {
+        login(): Chainable<void>;
+      }
+    }
+  }
+  export {};
+  
